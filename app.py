3a4789652279f3b1ebd99c5886c81d2904d1e74e
Samiksha Shukla <152069982+samiksha0shukla@users.noreply.github.com>
2024-06-20 08:48:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import helper
+from joblib import load
+
+with open('rf_model.joblib', 'rb') as file:
+    model = load(file)
+
+
+st.header('Duplicate Question Pairs')
+
+q1 = st.text_input('Enter question 1')
+q2 = st.text_input('Enter question 2')
+
+if st.button('Find'):
+    query = helper.query_point_creator(q1,q2)
+    result = model.predict(query)[0]
+
+    if result:
+        st.header('Duplicate')
+    else:
+        st.header('Not Duplicate')
